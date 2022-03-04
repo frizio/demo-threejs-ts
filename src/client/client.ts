b33a8e0e830ba22c1739fbf855999fe9f58748ad
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-03-04 18:52:09 +0100

    Importing OrbitControls Three.js Modules

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
@@ -13,6 +14,8 @@ camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
@@ -34,8 +37,8 @@ function onWindowResize() {
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01; //
+  cube.rotation.y += 0.01; //
 
   render();
 }
